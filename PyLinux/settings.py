bbc7302306c758ec0d4d1b8356457cc5758b48b4
wx <123@qq.com>
2021-07-20 15:52:45 +0800

    增加k8s发布配置

--- PyLinux/settings.py
+++ PyLinux/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'PyLinux.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'django_app',
+                'USER': 'root',
+                'PASSWORD': '123456',
+                'HOST': 'sft-mysql',
+		#'HOST': '10.244.1.39',
+                'PORT': '3306',
+                'OPTIONS': {'charset': 'utf8mb4'},
 	}
 }
 
